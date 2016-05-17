9c7c687d6d21d78b574eb9791fc6ec141ff3dcb6
LHS Compsci <lhscompsci@lh-l703-11-7364.ci.lexington.ma.us>
2016-05-17 14:22:17 -0400

    Game.java almost done

--- Game.java
+++ Game.java
@@ -7,9 +7,23 @@ public abstract class Game
 {
     private List<Player> players;
     private List<Controller> controllers;
+    private int[] score;
+
+    private boolean teamOut(int i)
+    {
+        return players.get(i % 2).out() &&
+               players.get(i % 2 + 2).out() &&
+               players.get(i % 2 + 4).out();
+    }
 
     private int evaluateQuestion(Question q)
     {
+        // not a remaining player
+        if (players.get(q.target()).out())
+            return -1;
+        // not a card
+        if (!q.card().isCard())
+            return -1;
         // asks same team
         if (q.asker() % 2 == q.target() % 2)
             return -1;
@@ -35,14 +49,18 @@ public abstract class Game
 
     private int evaluateDeclaration(Declaration d)
     {
+        int id = d.getQuestion(0).asker();
         d.setWork(true);
         for (int i = 0; i < 6; i++)
         {
             Question q = d.getQuestion(i);
+            // not a card
+            if (!q.card().isCard())
+                return -1;
             // declares for the other team
             if (q.asker() % 2 != q.target() % 2)
                 return -1;
-            // incorrect declaration
+
             if (!players.get(q.target()).gotdem(q.card()))
                 d.setWork(false);
 
@@ -55,11 +73,16 @@ public abstract class Game
                 }
             }
         }
+        if (d.worked())
+            score[id % 2]++;
+        else
+            score[(id + 1) % 2]++;
     }
 
     public Game(List<Player> p, List<Controller> c)
     {
         players = p;
         controllers = c;
+        score = new int[2];
     }
 }
