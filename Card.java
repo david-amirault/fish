85abf9cf131827b7783b12b0a749bc33f6d74704
unknown <david-amirault@users.noreply.github.com>
2016-05-17 17:38:49 -0400

    Game finished

--- Card.java
+++ Card.java
@@ -64,13 +64,13 @@ public class Card {
     }
 
     public boolean isCard() {
-        if (suit != “Clubs” && suit != “Diamonds” && suit != “Hearts” && suit != “Spades”)
+        if (suit != "Clubs" && suit != "Diamonds" && suit != "Hearts" && suit != "Spades")
             return false;
 
-        if (((rank != “2” && rank != “3” && rank != “4” && rank != “5” &&
-              rank != “6” && rank != “7”) || highHalf) &&
-            ((rank != “9” && rank != “10” && rank != “Jack” && rank != “Queen” &&
-              rank != “King” && rank != “Ace”) || !highHalf))
+        if (((rank != "2" && rank != "3" && rank != "4" &&
+              rank != "5" && rank != "6" && rank != "7") || highHalf) &&
+            ((rank != "9" && rank != "10" && rank != "Jack" &&
+              rank != "Queen" && rank != "King" && rank != "Ace") || !highHalf))
             return false;
 
         return true;
--- Game.java
+++ Game.java
@@ -16,39 +16,11 @@ public abstract class Game
                players.get(i % 2 + 4).out();
     }
 
-    private int evaluateQuestion(Question q)
-    {
-        // not a remaining player
-        if (players.get(q.target()).out())
-            return -1;
-        // not a card
-        if (!q.card().isCard())
-            return -1;
-        // asks same team
-        if (q.asker() % 2 == q.target() % 2)
-            return -1;
-        // not in this half-suit
-        if (players.get(q.asker()).halfsuits()[q.card().code()] == 0)
-            return -1;
-
-        int id = q.target();
-        // they actually have it
-        if (players.get(q.target()).gotdem(q.card()))
-        {
-            q.setWork(true);
-            id = q.asker();
-            players.get(q.target()).lose(q.card());
-            players.get(q.asker()).acquire(q.card());
-        }
-        // tell the Controllers what happened
-        for (Controller c : controllers)
-            c.hearQuestion(q);
-
-        return id;
-    }
-
     private int evaluateDeclaration(Declaration d)
     {
+        if (d.noGuess())
+            return 6; // code for not declaring
+
         int id = d.getQuestion(0).asker();
         d.setWork(true);
         for (int i = 0; i < 6; i++)
@@ -77,6 +49,47 @@ public abstract class Game
             score[id % 2]++;
         else
             score[(id + 1) % 2]++;
+
+        if (teamOut(id))
+            id = (id + 1) % 6;
+
+        if (teamOut(id))
+            return 7; // code for game over
+
+        while (player.get(id).out())
+            id = (id + 2) % 6;
+
+        return id;
+    }
+
+    private int evaluateQuestion(Question q)
+    {
+        // not a remaining player
+        if (players.get(q.target()).out())
+            return -1;
+        // not a card
+        if (!q.card().isCard())
+            return -1;
+        // asks same team
+        if (q.asker() % 2 == q.target() % 2)
+            return -1;
+        // not in this half-suit
+        if (players.get(q.asker()).halfsuits()[q.card().code()] == 0)
+            return -1;
+
+        int id = q.target();
+        // they actually have it
+        if (players.get(q.target()).gotdem(q.card()))
+        {
+            q.setWork(true);
+            id = q.asker();
+            players.get(q.target()).lose(q.card());
+            players.get(q.asker()).acquire(q.card());
+        }
+        for (Controller c : controllers)
+            c.hearQuestion(q);
+
+        return id;
     }
 
     public Game(List<Player> p, List<Controller> c)
@@ -84,5 +97,32 @@ public abstract class Game
         players = p;
         controllers = c;
         score = new int[2];
+        int id = 0, tmp;
+        while (true)
+        {
+            tmp = evaluateDeclaration(controllers.get(id).declare(teamOut(id + 1)));
+            while (tmp == -1)
+            {
+                System.out.println("Invalid Declaration!");
+                tmp = evaluateDeclaration(controllers.get(id).declare(teamOut(id + 1)));
+            }
+            if (tmp != 6)
+            {
+                if (tmp == 7)
+                    break;
+
+                id = tmp;
+                continue;
+            }
+
+            tmp = evaluateQuestion(controllers.get(id).ask());
+            while (tmp == -1)
+            {
+                System.out.println("Invalid Question!");
+                tmp = evaluateQuestion(controllers.get(id).ask());
+            }
+            id = tmp;
+        }
+        System.out.print("\nGood game!\nScore: " + score[0] + " to " + score[1] + ".");
     }
 }
